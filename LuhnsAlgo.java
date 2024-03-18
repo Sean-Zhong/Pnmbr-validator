6e0315a88769a71e4ac58c72d549b840aeff918e
Sean <sean.zhong98@gmail.com>
2024-03-18 15:21:36 +0100

    luhns algo wip

--- LuhnsAlgo.java
+++ LuhnsAlgo.java
@@ -1,5 +1,12 @@
 public class LuhnsAlgo {
-    public static Boolean validLuhns(int[] pnmbr) {
+    public static Boolean validLuhns(String pnmbr) {
+        String pnmbrDigits = pnmbr.replaceAll("[^\\d.]", "");
+        String tenDigitPnmbr = pnmbrDigits.substring(pnmbrDigits.length()-10, pnmbrDigits.length());
+        int[] numbers = tenDigitPnmbr.chars()
+                     .map(Character::getNumericValue)
+                     .toArray();
+        
+        // for loop with luhn calcs
         return false;
     }
 }
